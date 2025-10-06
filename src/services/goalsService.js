92974977011f72342300fc7fb1cab9fcb8cc6628
Henrique Nunes <henriquenunesmagalha@gmail.com>
2025-10-05 22:04:34 -0300

    goals-commit

---
+++ src/services/goalsService.js
@@ -0,0 +1,15 @@
+import { Client } from "stompjs";
+
+let stompClient;
+
+const connectGoals = (setTarefa) => {
+    let client = new Client({
+        brokerURL: "ws://localhost:8080/ws",
+        onConnect: () => {
+            client.subscribe('topic/goals', (message) => {
+
+            })
+        }
+    })
+}
+
